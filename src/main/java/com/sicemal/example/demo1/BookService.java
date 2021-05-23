0955ddf136495b642faa2340d7521d61fe8a8f21
Silvio Limeira <sicemal@gmail.com>
2021-05-23 12:13:48 -0300

    Lambda expression comparator for sort list - Get sorted list of books

--- src/main/java/com/sicemal/example/demo1/BookService.java
+++ src/main/java/com/sicemal/example/demo1/BookService.java
@@ -1,22 +1,15 @@
 package com.sicemal.example.demo1;
 
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
+
 
 public class BookService {
 	
 	public List<Book> getBooksInSort() {
 		List<Book> books = new BookDAO().getBooks();
-		// Anonymous implementation Comparator - Get sorted list of books
-		Collections.sort(books, new Comparator<Book>() {
-
-			@Override
-			public int compare(Book o1, Book o2) {
-				return o1.getName().compareTo(o2.getName());
-			}
-			
-		});
+		// Lambda expression comparator for sort list - Get sorted list of books
+		Collections.sort(books, (o1, o2) -> o1.getName().compareTo(o2.getName())); 
 		return books;
 	}
 	
