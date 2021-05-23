6094ce5c582af534f9153ed456f72452bb6958b8
Silvio Limeira <sicemal@gmail.com>
2021-05-23 12:52:16 -0300

    Implement Predicate Example with Lambda Expression and Filter

--- src/main/java/com/sicemal/example/demo1/functional/PredicateDemo.java
+++ src/main/java/com/sicemal/example/demo1/functional/PredicateDemo.java
@@ -1,5 +1,7 @@
 package com.sicemal.example.demo1.functional;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class PredicateDemo {
@@ -16,10 +18,18 @@ public class PredicateDemo {
 			}
 		};
 		*/
-		Predicate<Integer> predicate = (t) -> t % 2 == 0;
-
+		System.out.println("=== Example ===");
+		Predicate<Integer> predicate = t -> t % 2 == 0;
 		System.out.println(predicate.test(4));
 		System.out.println(predicate.test(7));
+		
+		List<Integer> list = Arrays.asList(1,2,3,4,5);
+		System.out.println("=== Example - List even numbers ===");
+		list.stream().filter(predicate).forEach(t -> System.out.println("print Even: " + t));
+		
+		System.out.println("=== Example 1 - List even numbers using lambda expression with filter ===");
+		list.stream().filter(t -> t % 2 == 0).forEach(t -> System.out.println("print Even: " + t));		
+		
 	}
 
 }
