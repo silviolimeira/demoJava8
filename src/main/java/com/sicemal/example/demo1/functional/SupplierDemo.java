6662fd93a6428c60a1a6225767721264fbb1b969
Silvio Limeira <sicemal@gmail.com>
2021-05-23 13:15:43 -0300

    Example Supplier with Lambda Expression with stream.findAny()

--- src/main/java/com/sicemal/example/demo1/functional/SupplierDemo.java
+++ src/main/java/com/sicemal/example/demo1/functional/SupplierDemo.java
@@ -1,16 +1,32 @@
 package com.sicemal.example.demo1.functional;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Supplier;
 
 public class SupplierDemo {
 
 	public static void main(String[] args) {
 		
-		Supplier<String> supplier = () -> {
-			return "Hi Java Techie";
-		};
-		
+//		Supplier<String> supplier = () -> {
+//			return "Hi Java Techie";
+//		};
+		Supplier<String> supplier = () -> "Hi Java Techie";
 		System.out.println("Supplier Demo: " + supplier.get());
+		
+		System.out.println("Example 2 ===========");
+		List<String> list = new ArrayList<>();
+		System.out.println(list.stream().findAny().orElseGet(supplier));
+		System.out.println("Example 3 orElseGet with lambda ===========");
+		// orElseGet with Lambda
+		System.out.println(list.stream().findAny().orElseGet(() -> "No content"));
+		
+		System.out.println("Example 4 orElseGet with lambda ===========");
+		list.add("One");
+		list.add("Two");
+		list.add("Three");
+		System.out.println(list.stream().findAny().orElseGet(() -> "No content"));
 		
 	}
 
