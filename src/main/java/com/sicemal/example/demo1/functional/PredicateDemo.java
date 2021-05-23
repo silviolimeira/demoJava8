d8667b8c337df4be7302827cb219da006a62f1cf
Silvio Limeira <sicemal@gmail.com>
2021-05-23 12:44:01 -0300

    Implement Predicate Example with Lambda Expression

--- src/main/java/com/sicemal/example/demo1/functional/PredicateDemo.java
+++ src/main/java/com/sicemal/example/demo1/functional/PredicateDemo.java
@@ -2,19 +2,22 @@ package com.sicemal.example.demo1.functional;
 
 import java.util.function.Predicate;
 
-public class PredicateDemo implements Predicate<Integer> {
+public class PredicateDemo {
 
-	@Override
-	public boolean test(Integer t) {
-		if (t % 2 == 0) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
 	public static void main(String[] args) {
-		Predicate<Integer> predicate = new PredicateDemo();
+
+		/*
+		// example using lambda expression
+		Predicate<Integer> predicate = 	(Integer t) -> {
+			if (t % 2 == 0) {
+				return true;
+			} else {
+				return false;
+			}
+		};
+		*/
+		Predicate<Integer> predicate = (t) -> t % 2 == 0;
+
 		System.out.println(predicate.test(4));
 		System.out.println(predicate.test(7));
 	}
