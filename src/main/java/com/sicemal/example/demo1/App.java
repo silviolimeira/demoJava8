c79e155e73c47c82c21dcf4f9a3ee33b90a03942
Silvio Limeira <sicemal@gmail.com>
2021-05-23 11:35:59 -0300

    Implement interface using lambda expression with parameters and result return

--- src/main/java/com/sicemal/example/demo1/App.java
+++ src/main/java/com/sicemal/example/demo1/App.java
@@ -1,8 +1,7 @@
 package com.sicemal.example.demo1;
 
 interface Calculator {
-	//void switchOn();
-	void sum(int a, int b);
+	int subtract(int a, int b);
 }
 
 public class App 
@@ -10,11 +9,12 @@ public class App
 
 	public static void main(String[] args) {
 		
-		// Implement interface using lambda expression with parameters
+		// Implement interface using lambda expression with parameters and result return
 		Calculator calculator = (a, b) -> {
-			System.out.println("Sum: " + (a + b));
+			return (a - b);
 		};
-		calculator.sum(2,3);
+		System.out.println("Result (8-3): " + calculator.subtract(8,3));
+		
 		
 	}
 	
